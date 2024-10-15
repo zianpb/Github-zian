10eeef4ef3d385b14f868f97463f3119914da947
zianpb <zianpb@gmail.com>
2024-10-15 10:45:43 +0700

    add index.html file

---
+++ ALCENT/CATATAN JAVASCRIPT.js
@@ -0,0 +1,22 @@
+
+// alert('Ini buat pop up atas ya')
+//         document.write('ini ditampilakn di halaaman web')
+//         console.log('ini ditampiilkan di console')
+//             contoh gesss
+//             var x = 'nama kamu'
+//             console.log(x); tulisan namanya manggil nama grup dari var nama
+//                                console log itu berfungsi buat munculin nilai ke consolenya
+            // prompt = buat input data 
+                
+// // `=========== Variable nama replace =============
+//     Var  nama= 'gr'
+//     nama= 'juljul 
+//     alert(nama) yang munculnya 'juljul'
+
+
+
+// ========================pengulangan==============
+// while,do,for
+// for (var i = 0; i < 1000; i++){
+//     document.write('Halo semuanya! <br>');
+// }
---
+++ ALCENT/While.js
@@ -0,0 +1,18 @@
+// var ngulang = true;
+// while(ngulang){
+//     console.log('helo')
+//     ngulang = confirm('lagi santaik kawan')
+// }
+
+// var jmlRuangan = 5;
+// var noRuangan = 1;
+
+// while (noRuangan <= jmlRuangan) {
+//     console.log(`Speaker di ruanagan ${noRuangan} berfungsi dengan baik.` );
+//     noRuangan++;
+// for ()
+// }
+
+let x = =   10  ;  
+if x = 5
+    document.write(agra fatang)
